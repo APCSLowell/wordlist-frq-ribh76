0689dc0ea84990b742e11ee38abd73c60101c5e8
ribh76 <ribhat@s.sfusd.edu>
2025-04-08 15:30:59 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,20 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int counter = 0; 
+    for (int i = 0; i<myList.size(); i++){
+      if ((myList.get(i)).length() == len){
+        counter++; 
+      }
+    } return counter; 
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
-  }
+    for (int i = 0; i<myList.size(); i++){
+      if ((myList.get(i)).length() == len){
+        myList.remove(i); i--; 
+      }
+    }
 }
